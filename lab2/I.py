bbae8c9e2c3bd0ef660dc42b68a9c23d06837866
grand <uali03.alm@gmail.com>
2022-02-11 21:53:50 +0600

    Second lab

---
+++ lab2/I.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+if 1 <= n <= 100:
+    given_discs = []
+    taken_disks = []
+    index = 0
+
+    operation = input()
+    if operation != 2:
+        given_discs.append(operation.split(" ")[1])
+    else:
+        taken_disks.append(given_discs[index])
+        index += 1
+
+    print_answer = ""
+
+    for d in taken_disks:
+        print_answer += d + " "
+
+    print(print_answer)
