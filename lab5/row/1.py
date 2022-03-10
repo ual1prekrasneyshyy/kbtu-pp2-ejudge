339562432e754501542df2e68569f26075971486
grand <uali03.alm@gmail.com>
2022-03-10 17:08:38 +0600

    Loading

--- lab5/row/1.py
+++ lab5/row/1.py
@@ -1,16 +1,16 @@
 import re
 import csv
 
-with open('receipt.txt', 'r') as f:
+with open('receipt.txt', 'r', encoding='utf-8') as f:
     txt = f.read()
 
 # txt = "null"
 
 table = [['order', 'name', 'count', 'price', 'total_price']]
 
-product_patterns = r'<order>.+.\n<name>.+\n<count>.+ x <price>.+\n<total_price>.+'
+# product_patterns =
 
-products_res = re.finditer(product_patterns, txt)
+products_res = re.finditer(r'(?P<order>\d+).\n(?P<name>.+)\n(?P<count>\S+) x (?P<price>\S+)\n(?P<total_price>.+)', txt)
 
 for res in products_res:
     table.append([
@@ -20,6 +20,8 @@ for res in products_res:
         res.group('price').strip(),
         res.group('total_price').strip()
     ])
+
+# print(table)
 
 with open('products.csv', 'w') as f:
     writer = csv.writer(f)
