3fe2c80486fe126c0e5f7df6e7e12616f135f368
grand <uali03.alm@gmail.com>
2022-03-17 16:08:36 +0600

    Loading

--- lab6/builtin-functions/2.py
+++ lab6/builtin-functions/2.py
@@ -3,9 +3,9 @@ s = input("Write a string:\n")
 uppers, lowers = 0, 0
 
 for c in s:
-    if ord(c) in range(65, 90):  #A-Z
+    if ord(c) in range(65, 91):  #A-Z
         uppers += 1
-    elif ord(c) in range(97, 122): #a-z
+    elif ord(c) in range(97, 123): #a-z
         lowers += 1
 
 print(f'There are {uppers} upper case letters and {lowers} lower case letters')
